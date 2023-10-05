ee7e8d59fb581636f093b53b72c1b132bc4d0e16
Pariterre <benjamin.michaud.phq@gmail.com>
2023-10-04 20:50:31 -0400

    ready for shipping

--- lib/main.dart
+++ lib/main.dart
@@ -27,7 +27,7 @@ class MyApp extends StatelessWidget {
                     arguments: twitchManager);
               },
               mockOptions: const TwitchMockOptions(
-                  isActive: true,
+                  isActive: false,
                   messagesFollowers: ['!spin'],
                   messagesModerators: ['!spin']),
               appInfo: TwitchAppInfo(
